8f6d9e11d9e287361947aeadba87e91d6855d295
MuniS67 <tursunovmunis34@gmail.com>
2024-05-12 18:37:29 +0500

    fixed

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,10 @@
-import React from "preact/compat";
 import "./app.css";
 
-export function App() {
-  const baseUrl: string = import.meta.env.VITE_BASE_URL;
-  const [todo, setTodo] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch(baseUrl + "/tasks")
-        .then((res) => res.json())
-        .then((res) => setTodo(res));
-  });
+export function App() {
+  
   return (
-  <div className={"w-full "}>
-    <h1>{todo.length}</h1>
-  </div>
+  <>
+  </>
   )
 }
