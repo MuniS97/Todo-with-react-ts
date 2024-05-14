3038f4554ba0656b3ec2727ca96dedc242a9d9cb
MuniS67 <tursunovmunis34@gmail.com>
2024-05-14 14:57:44 +0500

    added

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,16 @@ import { TaskForm } from "./components/TaskForm";
 import React from "preact/compat";
 import { TaskRender } from "./components/TaskRender";
 
+interface ITask {
+  id: number
+  title: string
+  description: string
+  checked: boolean
+}
 
 export function App() {
   const baseUrl = import.meta.env.VITE_BASE_URL;
-  const [tasks, setTasks] = React.useState([]);
+  const [tasks, setTasks] = React.useState<ITask[]>([]);
 
   React.useEffect(() => {
     fetch(baseUrl + "/tasks").then(res => res.json()).then(res => {
@@ -32,12 +38,12 @@ export function App() {
           "w-full max-w-[700px] min-h-[200px] h-full shadow-lg rounded-lg mx-auto p-[10px] flex flex-wrap gap-5 items-start justify-center"
         }
       >
-        {/* {tasks ? {
+        {tasks.length ? (
             
-            // tasks.map(item => (
-            //     <TaskRender Id={item.id} Checked={item.checked} Description={item.description} Title={item.title}/>
-            // ))
-             } : <span>Loading.....</span>} */}
+            tasks.map((item: ITask) => (
+                <TaskRender Id={item.id} Checked={item.checked} Description={item.description} Title={item.title}/>
+            ))
+             ) : <span>Loading.....</span>}
       </div>
     </>
   );
